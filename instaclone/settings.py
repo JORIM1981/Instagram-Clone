d2c01796d0dcdc2a02ef2854e84eff58fd781196
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-07-05 13:46:16 +0300

    add changes in models.py

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -80,11 +80,12 @@ WSGI_APPLICATION = 'instaclone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instaclone',
+        'USER': 'midumbi',
+    'PASSWORD':'midumbi',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
